29c49ee1eb508e2fea001b23b0a687677bb60999
Kathy Hoang <kathyhahoang@gmail.com>
2024-04-19 12:16:50 -0500

    Hightlight potential color changes in layout

--- src/home/HomeServices.tsx
+++ src/home/HomeServices.tsx
@@ -11,12 +11,15 @@ function HomeServices() {
         </div>
         <div className={styles["text-container"]}>
           <div className={styles["text-container-center"]}>
-            <h3>We guarantee:</h3>
+            <h3>
+              Whether your dry eye symptoms are mild or severe, we will
+              continuously work with you until we achieve the highest level of
+              vision and comfort attainable. Our dry eye services include:
+            </h3>
             <ul>
-              <li>High quality eye exam</li>
-              <li>Dry eye evaluation with management</li>
-              <li>Expert contact lens fittings</li>
-              <li>Eyeglasses</li>
+              <li>- Initial evaluations</li>
+              <li>- Long-term management</li>
+              <li>- Latest treatments in dry eye disease</li>
             </ul>
           </div>
         </div>
@@ -27,15 +30,12 @@ function HomeServices() {
         </div>
         <div className={styles["text-container"]}>
           <div className={styles["text-container-center"]}>
-            <h3>
-              Whether your dry eye symptoms are mild or severe, we will
-              continuously work with you until we achieve the highest level of
-              vision and comfort attainable. Our dry eye services include:
-            </h3>
+            <h3>We guarantee:</h3>
             <ul>
-              <li>Initial evaluations</li>
-              <li>Long-term management</li>
-              <li>Latest treatments in dry eye disease</li>
+              <li>- High quality eye exam</li>
+              <li>- Dry eye evaluation with management</li>
+              <li>- Expert contact lens fittings</li>
+              <li>- Eyeglasses</li>
             </ul>
           </div>
         </div>
