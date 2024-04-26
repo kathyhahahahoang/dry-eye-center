562babaf7637107a1a6fb92b35b91ae8a322248e
Kathy Hoang <kathyhahoang@gmail.com>
2024-04-26 11:58:54 -0500

    Fix AppLayout bug

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./ui/NavBar";
-import Footer from "./ui/Footer";
+import NavBar from "../src/ui/NavBar";
+import Footer from "../src/ui/Footer";
 
 function AppLayout() {
   return (
