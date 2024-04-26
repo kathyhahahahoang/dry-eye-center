ce4f1a4c46c983c38a45561537583765fa6b7b94
Kathy Hoang <kathyhahoang@gmail.com>
2024-04-26 11:20:51 -0500

    Remove social media icons from Footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -4,13 +4,18 @@ import logo from "../assets/images/logo.png";
 function Footer() {
   return (
     <div className={styles.container}>
-      <div className={styles["logo-container"]}>
-        <img src={logo} className={styles.logo} />
-        <h3>
-          Dry Eye Center
-          <br />
-          of San Antonio
-        </h3>
+      <div className={styles["left-container"]}>
+        <div className={styles["logo-container"]}>
+          <img src={logo} className={styles.logo} />
+          <h3>
+            Dry Eye Center
+            <br />
+            of San Antonio
+          </h3>
+        </div>
+        <p className={styles.copyright}>
+          &copy; 2024 Dry Eye Center of San Antonio
+        </p>
       </div>
       <div className={styles["links-container"]}>
         <ul className={styles["ul-container"]}>
@@ -27,26 +32,6 @@ function Footer() {
           <li>Friday: 9am - 3pm</li>
           <li>Saturday: closed</li>
           <li>Sunday: closed</li>
-        </ul>
-        <ul className={styles["ul-container"]}>
-          <li>Connect with us:</li>
-          {/* <li>Phone: (210)-403-9050</li>
-          <li>Fax: (210)-403-9939</li>
-          <li>alamoeyecare@gmail.com</li> */}
-          <div className={styles["social-media-container"]}>
-            <div>
-              <RiInstagramLine className={styles.icon} />
-            </div>
-            <div>
-              <RiTwitterLine className={styles.icon} />
-            </div>
-            <div>
-              <RiTwitterXLine className={styles.icon} />
-            </div>
-            <div>
-              <RiFacebookBoxLine className={styles.icon} />
-            </div>
-          </div>
         </ul>
       </div>
     </div>
