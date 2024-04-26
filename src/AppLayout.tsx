5279e64a3f97a4526fbe40ef8ddc9f53260c42e4
Kathy Hoang <kathyhahoang@gmail.com>
2024-04-26 13:50:07 -0500

    Remove NavBar from AppLayout

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./ui/NavBar.tsx";
+
 import Footer from "./ui/Footer.tsx";
 
 function AppLayout() {
   return (
     <div>
-      <NavBar />
       <main>
         <Outlet />
       </main>
