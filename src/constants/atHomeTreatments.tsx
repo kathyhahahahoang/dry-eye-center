f482d978dca07c2c04919da0d09bfdcd1d77485e
Kathy Hoang <kathyhahoang@gmail.com>
2024-04-27 15:46:25 -0500

    Create layout for AtHomeTreatments

--- src/constants/atHomeTreatments.tsx
+++ src/constants/atHomeTreatments.tsx
@@ -1,3 +1,41 @@
+type atHomeTreatmentsTypes = {
+  title: string;
+  desc1: string;
+  desc2?: string;
+  desc3?: string;
+};
+
+export const atHomeTreatments: atHomeTreatmentsTypes[] = [
+  {
+    title: "Hot compress and massage",
+    desc1:
+      "If you are diagnosed with meibomian gland dysfunction, we may ask you to perform hot compresses and lid massage for relief of symtoms. This is performed to help melt meibomian secretions that are blocking the glands. Lid massage helps to encourage the meibum out of the glands. When they are able to drain, the glands can function properly.  ",
+    desc2:
+      "Hot compresses and lid massage can often be ineffective if the glands are completely blocked. So, we will often perform meibomian expression as an in-house treatment to completely unblock the glands prior to asking you to perform the treatment at home.",
+    desc3:
+      "We will show you how to do the hot compresses and lid massage correctly.",
+  },
+  {
+    title: "Intensive lid hygiene",
+    desc1:
+      "At home, you can add a simple daily regimen to your bathroom cycle: cleaning your eyelids. In order to keep your dry eye symptoms and inflammation under control, you need to have a clean environment for the tears to work in. ",
+    desc2:
+      "If our eyes are not cleaned properly, bacteria that live around the eye become overactive and release toxins that irritate the eye. Also, debris from the day blocks the meibomian glands causing meibomian gland dysfunction if not cleaned properly.",
+    desc3:
+      "We may ask you to carry out intensive lid hygeine to keep the lids and lid margins clean. We do not recommend baby shampoo as this has been shown to destroy the lipid layer of the tears. We will supply you with lid wipes or proper lid scrubbing solutions and teach you how to clean your lids properly.",
+  },
+  {
+    title: "Blinking exercises",
+    desc1:
+      "Blinking is essential to having a healthy eye surface. It decreases deposits, allows for tear movement, and smooths the surface for optimal vision. With the rapid growing technology advancements, it is hard to avoid digital screens in everyday life. While looking at digital devices, the blink rate decreases significantly. Introducing these exercises can help your eyes remember to blink more often and keep your ocular surface moist.",
+  },
+  {
+    title: "Ocular Nutrition",
+    desc1:
+      "Omega-3 vitamins, or essential fatty acids, can be added to your diet for dry eye relief.  They are found naturally in oily fish and flaxseed. Ask your optometrist if these supplements are right for you.",
+  },
+];
+
 export const hotCompress = {
   title: "Hot compress and massage",
   desc1:
--- src/pages/AtHomeTreatments.tsx
+++ src/pages/AtHomeTreatments.tsx
@@ -1,5 +1,31 @@
+import styles from "./AtHomeTreatments.module.scss";
+import glasses from "../assets/images/glasses.jpg";
+import { atHomeTreatments } from "../constants/atHomeTreatments";
+
 function AtHomeTreatments() {
-  return <div>AT HOME TREATMMENTS</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <img src={glasses} className={styles["header-picture"]} />
+        <h1>At-home treatments</h1>
+      </div>
+      {atHomeTreatments.map((el, i) => {
+        return (
+          <div className={styles["treatment-container"]} key={i}>
+            <div className={styles["treatment-picture"]}>
+              <img src={glasses} />
+            </div>
+            <div className={styles["treatment-text"]}>
+              <h3>{el.title}</h3>
+              <p>{el.desc1}</p>
+              {el.desc2 && <p>{el.desc2}</p>}
+              {el.desc3 && <p>{el.desc3}</p>}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default AtHomeTreatments;
