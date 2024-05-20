1db075f203f293038f5ee4f2855f8661b22c49b4
Kathy Hoang <kathyhahoang@gmail.com>
2024-05-20 14:56:56 -0500

    Change layout for treatments to half page squares

--- src/pages/AtHomeTreatments.tsx
+++ src/pages/AtHomeTreatments.tsx
@@ -1,30 +1,28 @@
 import styles from "./AtHomeTreatments.module.scss";
 import glasses from "../assets/images/glasses.jpg";
 import { atHomeTreatments } from "../constants/atHomeTreatmentsInfo";
+import Banner from "../ui/Banner";
 
 function AtHomeTreatments() {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        {/* <img src={glasses} className={styles["header-picture"]} /> */}
-        <h1 className={styles.title}>At-home treatments</h1>
-      </div>
+      <Banner
+        title="At-home treatments"
+        desc=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, aliquid. Id nostrum aperiam odio"
+        bgColor="var(--color-blue-extra-light"
+      />
       {atHomeTreatments.map((el, i) => {
         return (
           <div className={styles["treatment-container"]} key={i}>
-            <div className={"row"}>
-              <div className={"col-1-of-3"}>
-                <div className={styles["treatment-picture"]}>
-                  <img src={glasses} />
-                </div>
-              </div>
-              <div className={"col-2-of-3"}>
-                <div className={styles["treatment-text"]}>
-                  <h3>{el.title}</h3>
-                  <p>{el.desc1}</p>
-                  {el.desc2 && <p>{el.desc2}</p>}
-                  {el.desc3 && <p>{el.desc3}</p>}
-                </div>
+            <div className={styles["treatment-picture"]}>
+              <img src={glasses} />
+            </div>
+            <div className={styles["treatment-details"]}>
+              <div className={styles["treatment-details-center"]}>
+                <h3>{el.title}</h3>
+                <p>{el.desc1}</p>
+                {el.desc2 && <p>{el.desc2}</p>}
+                {el.desc3 && <p>{el.desc3}</p>}
               </div>
             </div>
           </div>
