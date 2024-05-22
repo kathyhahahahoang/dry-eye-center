aaa71c46bc97167597121a2e3795a99b2ef469b4
Kathy Hoang <kathyhahoang@gmail.com>
2024-05-22 11:28:06 -0500

    Create layout for Resources

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,12 +1,89 @@
 import styles from "./Resources.module.scss";
+import eyes from "../assets/images/eyes.jpg";
 
 function Resources() {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Resources</h1>
+      <div className={styles["video-header"]}>
+        <h1 className={styles.title}>Videos</h1>
       </div>
-      <div>RESOURCES</div>
+      <div className={styles["video-container"]}>
+        <div className={styles["video-item"]}>
+          <div className={styles["video-picture"]}>
+            <img src={eyes} />
+          </div>
+          <div className={styles["video-details"]}>
+            <p>How to use a hot compress at home</p>
+          </div>
+        </div>
+        <div className={styles["video-item"]}>
+          <div className={styles["video-picture"]}>
+            <img src={eyes} />
+          </div>
+          <div className={styles["video-details"]}>
+            <p>How to use a hot compress at home</p>
+          </div>
+        </div>
+        <div className={styles["video-item"]}>
+          <div className={styles["video-picture"]}>
+            <img src={eyes} />
+          </div>
+          <div className={styles["video-details"]}>
+            <p>How to use a hot compress at home</p>
+          </div>
+        </div>
+        <div className={styles["video-item"]}>
+          <div className={styles["video-picture"]}>
+            <img src={eyes} />
+          </div>
+          <div className={styles["video-details"]}>
+            <p>How to use a hot compress at home</p>
+          </div>
+        </div>
+        <div className={styles["video-item"]}>
+          <div className={styles["video-picture"]}>
+            <img src={eyes} />
+          </div>
+          <div className={styles["video-details"]}>
+            <p>How to use a hot compress at home</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles["article-header"]}>
+        <h1 className={styles.title}>Articles</h1>
+      </div>
+      <div className={styles["articles-container"]}>
+        <div className={styles["article-item"]}>
+          <h3>How to prevent dry eye disease</h3>
+          <div className={styles["article-picture"]}>
+            <img src={eyes} />
+          </div>
+        </div>
+        <div className={styles["article-item"]}>
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            veniam, tenetur
+          </h3>
+          <div className={styles["article-picture"]}>
+            <img src={eyes} />
+          </div>
+        </div>
+        <div className={styles["article-item"]}>
+          <h3>How to prevent dry eye disease</h3>
+          <div className={styles["article-picture"]}>
+            <img src={eyes} />
+          </div>
+        </div>
+        <div className={styles["article-item"]}>
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            veniam, tenetur
+          </h3>
+          <div className={styles["article-picture"]}>
+            <img src={eyes} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
