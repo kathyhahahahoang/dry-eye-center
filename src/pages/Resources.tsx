a92f93689dccf5d1dbaa21ba8b26ce26b6449d3e
Kathy Hoang <kathyhahoang@gmail.com>
2024-05-23 12:41:47 -0500

    Change Resource's article container to implement row class

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -53,34 +53,78 @@ function Resources() {
         <h1 className={`${styles.title} h1`}>Articles</h1>
       </div>
       <div className={styles["articles-container"]}>
-        <div className={styles["article-item"]}>
-          <h3 className={"h3"}>How to prevent dry eye disease</h3>
-          <div className={styles["article-picture"]}>
-            <img src={eyes} />
+        <div className={"row"}>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-title"]}>
+              <h4 className={"h4"}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                veniam, tenetur
+              </h4>
+            </div>
+          </div>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-picture"]}>
+              <img src={eyes} />
+            </div>
           </div>
         </div>
-        <div className={styles["article-item"]}>
-          <h3 className={"h3"}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            veniam, tenetur
-          </h3>
-          <div className={styles["article-picture"]}>
-            <img src={eyes} />
+        <div className={"row"}>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-title"]}>
+              <h4 className={"h4"}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi, rerum? Quis vel doloribus nostrum corrupti
+              </h4>
+            </div>
+          </div>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-picture"]}>
+              <img src={eyes} />
+            </div>
           </div>
         </div>
-        <div className={styles["article-item"]}>
-          <h3 className={"h3"}>How to prevent dry eye disease</h3>
-          <div className={styles["article-picture"]}>
-            <img src={eyes} />
+        <div className={"row"}>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-title"]}>
+              <h4 className={"h4"}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h4>
+            </div>
+          </div>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-picture"]}>
+              <img src={eyes} />
+            </div>
           </div>
         </div>
-        <div className={styles["article-item"]}>
-          <h3 className={"h3"}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-            veniam, tenetur
-          </h3>
-          <div className={styles["article-picture"]}>
-            <img src={eyes} />
+        <div className={"row"}>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-title"]}>
+              <h4 className={"h4"}>
+                Reprehenderit cum dignissimos praesentium repellat, ab corrupti
+                iure obcaecati odio suscipit na
+              </h4>
+            </div>
+          </div>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-picture"]}>
+              <img src={eyes} />
+            </div>
+          </div>
+        </div>
+        <div className={"row"}>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-title"]}>
+              <h4 className={"h4"}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                veniam, tenetur
+              </h4>
+            </div>
+          </div>
+          <div className={"col-2-of-4"}>
+            <div className={styles["article-picture"]}>
+              <img src={eyes} />
+            </div>
           </div>
         </div>
       </div>
@@ -89,3 +133,38 @@ function Resources() {
 }
 
 export default Resources;
+
+{
+  /* <div className={styles["articles-container"]}>
+<div className={styles["article-item"]}>
+  <h4 className={"h4"}>How to prevent dry eye disease</h4>
+  <div className={styles["article-picture"]}>
+    <img src={eyes} />
+  </div>
+</div>
+<div className={styles["article-item"]}>
+  <h4 className={"h4"}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+    veniam, tenetur
+  </h4>
+  <div className={styles["article-picture"]}>
+    <img src={eyes} />
+  </div>
+</div>
+<div className={styles["article-item"]}>
+  <h4 className={"h4"}>How to prevent dry eye disease</h4>
+  <div className={styles["article-picture"]}>
+    <img src={eyes} />
+  </div>
+</div>
+<div className={styles["article-item"]}>
+  <h4 className={"h4"}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+    veniam, tenetur
+  </h4>
+  <div className={styles["article-picture"]}>
+    <img src={eyes} />
+  </div>
+</div>
+</div> */
+}
