3b53c6731693fafda349023548caa4d831e9dd1c
Kathy Hoang <kathyhahoang@gmail.com>
2024-05-24 15:12:59 -0500

    Create resourcesInfo constant and update Resources to width-container instead of row className

---
+++ src/constants/resourcesInfo.tsx
@@ -0,0 +1,37 @@
+import home from "../assets/images/home.jpg";
+import elderly7 from "../assets/images/elderly7.jpg";
+
+export const videos = [
+  { title: "How to use a hot compress at home", picture: `${home}` },
+  { title: "How to use a hot compress at home", picture: `${home}` },
+  { title: "How to use a hot compress at home", picture: `${home}` },
+  { title: "How to use a hot compress at home", picture: `${home}` },
+  { title: "How to use a hot compress at home", picture: `${home}` },
+];
+
+export const articles = [
+  {
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam, tenetur",
+    picture: `${elderly7}`,
+  },
+  {
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, rerum? Quis vel doloribus nostrum corrupti",
+    picture: `${elderly7}`,
+  },
+  {
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    picture: `${elderly7}`,
+  },
+  {
+    title:
+      "Reprehenderit cum dignissimos praesentium repellat, ab corrupti iure obcaecati odio suscipit na",
+    picture: `${elderly7}`,
+  },
+  {
+    title:
+      "Reprehenderit cum repellat, ab corrupti iure obcaecati odio suscipit na",
+    picture: `${elderly7}`,
+  },
+];
--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -1,130 +1,45 @@
 import styles from "./Resources.module.scss";
 import eyes from "../assets/images/eyes.jpg";
+import { videos, articles } from "../constants/resourcesInfo";
 
 function Resources() {
   return (
     <div className={styles.container}>
-      <div className={styles["video-header"]}>
-        <h1 className={`${styles.title} h1`}>Videos</h1>
-      </div>
-      <div className={styles["video-container"]}>
-        <div className={styles["video-item"]}>
-          <div className={styles["video-picture"]}>
-            <img src={eyes} />
-          </div>
-          <div className={styles["video-details"]}>
-            <p>How to use a hot compress at home</p>
-          </div>
+      <div className={styles["video-section"]}>
+        <div className={styles["video-header"]}>
+          <h1 className={`${styles.title} h1`}>Videos</h1>
         </div>
-        <div className={styles["video-item"]}>
-          <div className={styles["video-picture"]}>
-            <img src={eyes} />
-          </div>
-          <div className={styles["video-details"]}>
-            <p>How to use a hot compress at home</p>
-          </div>
-        </div>
-        <div className={styles["video-item"]}>
-          <div className={styles["video-picture"]}>
-            <img src={eyes} />
-          </div>
-          <div className={styles["video-details"]}>
-            <p>How to use a hot compress at home</p>
-          </div>
-        </div>
-        <div className={styles["video-item"]}>
-          <div className={styles["video-picture"]}>
-            <img src={eyes} />
-          </div>
-          <div className={styles["video-details"]}>
-            <p>How to use a hot compress at home</p>
-          </div>
-        </div>
-        <div className={styles["video-item"]}>
-          <div className={styles["video-picture"]}>
-            <img src={eyes} />
-          </div>
-          <div className={styles["video-details"]}>
-            <p>How to use a hot compress at home</p>
+        <div className={"width-container"}>
+          <div className={styles["videos-container"]}>
+            {videos.map((el, i) => (
+              <div className={styles["video-item"]} key={i}>
+                <div className={styles["video-picture"]}>
+                  <img src={el.picture} />
+                </div>
+                <div className={styles["video-details"]}>
+                  <p className={"main-text"}>{el.title}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className={styles["article-header"]}>
-        <h1 className={`${styles.title} h1`}>Articles</h1>
-      </div>
-      <div className={styles["articles-container"]}>
-        <div className={"row"}>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-title"]}>
-              <h4 className={"h4"}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                veniam, tenetur
-              </h4>
-            </div>
-          </div>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-picture"]}>
-              <img src={eyes} />
-            </div>
-          </div>
+      <div className={styles["article-section"]}>
+        <div className={styles["article-header"]}>
+          <h1 className={`${styles.title} h1`}>Articles</h1>
         </div>
-        <div className={"row"}>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-title"]}>
-              <h4 className={"h4"}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi, rerum? Quis vel doloribus nostrum corrupti
-              </h4>
-            </div>
-          </div>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-picture"]}>
-              <img src={eyes} />
-            </div>
-          </div>
-        </div>
-        <div className={"row"}>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-title"]}>
-              <h4 className={"h4"}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </h4>
-            </div>
-          </div>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-picture"]}>
-              <img src={eyes} />
-            </div>
-          </div>
-        </div>
-        <div className={"row"}>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-title"]}>
-              <h4 className={"h4"}>
-                Reprehenderit cum dignissimos praesentium repellat, ab corrupti
-                iure obcaecati odio suscipit na
-              </h4>
-            </div>
-          </div>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-picture"]}>
-              <img src={eyes} />
-            </div>
-          </div>
-        </div>
-        <div className={"row"}>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-title"]}>
-              <h4 className={"h4"}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                veniam, tenetur
-              </h4>
-            </div>
-          </div>
-          <div className={"col-2-of-4"}>
-            <div className={styles["article-picture"]}>
-              <img src={eyes} />
-            </div>
+        <div className={"width-container"}>
+          <div className={styles["articles-container"]}>
+            {articles.map((el, i) => (
+              <div className={styles["article-item"]} key={i}>
+                <div className={styles["article-details"]}>
+                  <h4 className={"h4"}>{el.title}</h4>
+                </div>
+                <div className={styles["article-picture"]}>
+                  <img src={el.picture} />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
@@ -167,4 +82,83 @@ export default Resources;
   </div>
 </div>
 </div> */
+}
+
+{
+  /* <div className={styles["articles-container"]}>
+  <div className={"row"}>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-title"]}>
+        <h4 className={"h4"}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam,
+          tenetur
+        </h4>
+      </div>
+    </div>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-picture"]}>
+        <img src={eyes} />
+      </div>
+    </div>
+  </div>
+  <div className={"row"}>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-title"]}>
+        <h4 className={"h4"}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+          rerum? Quis vel doloribus nostrum corrupti
+        </h4>
+      </div>
+    </div>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-picture"]}>
+        <img src={eyes} />
+      </div>
+    </div>
+  </div>
+  <div className={"row"}>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-title"]}>
+        <h4 className={"h4"}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h4>
+      </div>
+    </div>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-picture"]}>
+        <img src={eyes} />
+      </div>
+    </div>
+  </div>
+  <div className={"row"}>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-title"]}>
+        <h4 className={"h4"}>
+          Reprehenderit cum dignissimos praesentium repellat, ab corrupti iure
+          obcaecati odio suscipit na
+        </h4>
+      </div>
+    </div>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-picture"]}>
+        <img src={eyes} />
+      </div>
+    </div>
+  </div>
+  <div className={"row"}>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-title"]}>
+        <h4 className={"h4"}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam veniam,
+          tenetur
+        </h4>
+      </div>
+    </div>
+    <div className={"col-2-of-4"}>
+      <div className={styles["article-picture"]}>
+        <img src={eyes} />
+      </div>
+    </div>
+  </div>
+</div>; */
 }
