2fde157996ba9277cb3ea30f6ea2f4d27494aec8
Kathy Hoang <kathyhahoang@gmail.com>
2024-05-28 15:14:04 -0500

    Create media queries for Footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -4,36 +4,33 @@ import logo from "../assets/images/logo.png";
 function Footer() {
   return (
     <div className={styles.container}>
-      <div>
-        <div className={"row"}>
-          <div className={"col-1-of-3"}>
-            <div className={styles["logo-container"]}>
-              <img src={logo} className={styles.logo} />
-              <p className={styles.name}>Dry Eye Center of San Antonio</p>
-            </div>
-          </div>
-          <div className={"col-2-of-3"}>
-            <div className={styles["links-container"]}>
-              <ul className={styles["ul-container"]}>
-                <li>Alamo Eye Care</li>
-                <li>1742 N. Loop 1604 E., Suite 117</li>
-                <li>San Antonio, TX 78232</li>
-              </ul>
-              <ul className={styles["ul-container"]}>
-                <li>Hours</li>
-                <li>Monday - Thursday: 9am - 6pm</li>
-                <li>Friday: 9am - 3pm</li>
-                <li>Saturday: closed</li>
-                <li>Sunday: closed</li>
-              </ul>
-              <ul className={styles["ul-container"]}>
-                <li>Contact us</li>
-                <li>Phone: (210)-403-9050</li>
-                <li>Fax: (210)-403-9939</li>
-                <li>alamoeyecare@gmail.com</li>
-              </ul>
-            </div>
-          </div>
+      <div className={`${styles["footer-container"]} width-container`}>
+        <div className={styles["logo-container"]}>
+          <img src={logo} className={styles.logo} />
+          <p className={styles.name}>
+            Dry Eye Center
+            <br /> of San Antonio
+          </p>
+        </div>
+        <div className={styles["links-container"]}>
+          <ul className={styles["ul-container"]}>
+            <li>Alamo Eye Care</li>
+            <li>1742 N. Loop 1604 E., Suite 117</li>
+            <li>San Antonio, TX 78232</li>
+          </ul>
+          <ul className={styles["ul-container"]}>
+            <li>Hours</li>
+            <li>Monday - Thursday: 9am - 6pm</li>
+            <li>Friday: 9am - 3pm</li>
+            <li>Saturday: closed</li>
+            <li>Sunday: closed</li>
+          </ul>
+          <ul className={styles["ul-container"]}>
+            <li>Contact us</li>
+            <li>Phone: (210)-403-9050</li>
+            <li>Fax: (210)-403-9939</li>
+            <li>alamoeyecare@gmail.com</li>
+          </ul>
         </div>
       </div>
       <p className={styles.copyright}>
@@ -44,3 +41,34 @@ function Footer() {
 }
 
 export default Footer;
+
+<div className={"row"}>
+  <div className={"col-1-of-3"}>
+    <div className={styles["logo-container"]}>
+      <img src={logo} className={styles.logo} />
+      <p className={styles.name}>Dry Eye Center of San Antonio</p>
+    </div>
+  </div>
+  <div className={"col-2-of-3"}>
+    <div className={styles["links-container"]}>
+      <ul className={styles["ul-container"]}>
+        <li>Alamo Eye Care</li>
+        <li>1742 N. Loop 1604 E., Suite 117</li>
+        <li>San Antonio, TX 78232</li>
+      </ul>
+      <ul className={styles["ul-container"]}>
+        <li>Hours</li>
+        <li>Monday - Thursday: 9am - 6pm</li>
+        <li>Friday: 9am - 3pm</li>
+        <li>Saturday: closed</li>
+        <li>Sunday: closed</li>
+      </ul>
+      <ul className={styles["ul-container"]}>
+        <li>Contact us</li>
+        <li>Phone: (210)-403-9050</li>
+        <li>Fax: (210)-403-9939</li>
+        <li>alamoeyecare@gmail.com</li>
+      </ul>
+    </div>
+  </div>
+</div>;
