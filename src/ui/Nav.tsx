471b25a688341ed5d65b73237eb3c9f7f0b49f93
Kathy Hoang <kathyhahoang@gmail.com>
2024-05-29 12:31:54 -0500

    Create Nav with hamburger menu option

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -45,22 +45,33 @@ function Nav() {
       ? `${styles.link} ${styles["link-scrolled"]}`
       : `${styles.link}`;
 
-  return (
-    <nav className={scrolledNav}>
-      <div className={styles.logo}>
-        <NavLink to="/">
-          <img src={logoBlack} className={styles.picture} />
-        </NavLink>
-      </div>
-      <div>
-        <NavLink to="/">
-          <p className={styles.name}>
-            Dry Eye Center of San Antonio at Alamo Eye Care
-          </p>
-        </NavLink>
+  const HamburgerNavBar = (
+    <nav className={styles["hamburger-container"]}>
+      <div className={styles["hamburger-nav"]}>
+        <div className={styles.logo}>
+          <NavLink to="/">
+            <img src={logoBlack} className={styles.picture} />
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/">
+            <p className={styles.name}>
+              Dry Eye Center of San Antonio at Alamo Eye Care
+            </p>
+          </NavLink>
+        </div>
+        <button
+          className={styles.hamburger}
+          onClick={() => {
+            setHamburgerOpen((cur) => !cur);
+          }}
+        >
+          {!hamburgerOpen && <RiMenuFill className={styles.icon} />}
+          {hamburgerOpen && <RiCloseFill className={styles.icon} />}
+        </button>
       </div>
       <ul
-        className={styles["nav-links-ul"]}
+        className={styles["hamburger-ul-container"]}
         style={
           hamburgerOpen
             ? {
@@ -71,6 +82,79 @@ function Nav() {
             : {}
         }
       >
+        <li>
+          <NavLink
+            to="/doctors"
+            className={({ isActive }) =>
+              isActive ? `${styles["link-active"]}` : `${scrolledLink}`
+            }
+            onClick={() => setHamburgerOpen(false)}
+          >
+            Our doctors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/dry-eye-disease"
+            className={({ isActive }) =>
+              isActive ? `${styles["link-active"]}` : `${scrolledLink}`
+            }
+            onClick={() => setHamburgerOpen(false)}
+          >
+            Dry eye disease
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/resources"
+            className={({ isActive }) =>
+              isActive ? `${styles["link-active"]}` : `${scrolledLink}`
+            }
+            onClick={() => setHamburgerOpen(false)}
+          >
+            Resources
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/at-home-treatments"
+            className={({ isActive }) =>
+              isActive ? `${styles["link-active"]}` : `${scrolledLink}`
+            }
+            onClick={() => setHamburgerOpen(false)}
+          >
+            At-home treatments
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/in-office-treatments"
+            className={({ isActive }) =>
+              isActive ? `${styles["link-active"]}` : `${scrolledLink}`
+            }
+            onClick={() => setHamburgerOpen(false)}
+          >
+            In-office treatments
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? `${styles["link-active"]}` : `${scrolledLink}`
+            }
+            onClick={() => setHamburgerOpen(false)}
+          >
+            Contact us
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+
+  const NavBar = (
+    <nav className={scrolledNav}>
+      <ul className={styles["nav-links-ul"]}>
         <div className={styles["nav-links-left"]}>
           <li>
             <NavLink
@@ -78,7 +162,6 @@ function Nav() {
               className={({ isActive }) =>
                 isActive ? `${styles["link-active"]}` : `${scrolledLink}`
               }
-              onClick={() => setHamburgerOpen(false)}
             >
               Our doctors
             </NavLink>
@@ -89,7 +172,6 @@ function Nav() {
               className={({ isActive }) =>
                 isActive ? `${styles["link-active"]}` : `${scrolledLink}`
               }
-              onClick={() => setHamburgerOpen(false)}
             >
               Dry eye disease
             </NavLink>
@@ -100,7 +182,6 @@ function Nav() {
               className={({ isActive }) =>
                 isActive ? `${styles["link-active"]}` : `${scrolledLink}`
               }
-              onClick={() => setHamburgerOpen(false)}
             >
               Resources
             </NavLink>
@@ -118,7 +199,6 @@ function Nav() {
               className={({ isActive }) =>
                 isActive ? `${styles["link-active"]}` : `${scrolledLink}`
               }
-              onClick={() => setHamburgerOpen(false)}
             >
               At-home treatments
             </NavLink>
@@ -129,7 +209,6 @@ function Nav() {
               className={({ isActive }) =>
                 isActive ? `${styles["link-active"]}` : `${scrolledLink}`
               }
-              onClick={() => setHamburgerOpen(false)}
             >
               In-office treatments
             </NavLink>
@@ -140,108 +219,21 @@ function Nav() {
               className={({ isActive }) =>
                 isActive ? `${styles["link-active"]}` : `${scrolledLink}`
               }
-              onClick={() => setHamburgerOpen(false)}
             >
               Contact us
             </NavLink>
           </li>
         </div>
       </ul>
-      <button
-        className={styles.hamburger}
-        onClick={() => {
-          setHamburgerOpen((cur) => !cur);
-        }}
-      >
-        {!hamburgerOpen && <RiMenuFill className={styles.icon} />}
-        {hamburgerOpen && <RiCloseFill className={styles.icon} />}
-      </button>
     </nav>
+  );
+
+  return (
+    <>
+      {isMobile && HamburgerNavBar}
+      {!isMobile && NavBar}
+    </>
   );
 }
 
 export default Nav;
-
-// import styles from "./Nav.module.scss";
-// import logo from "../assets/images/logo.png";
-// import { NavLink } from "react-router-dom";
-
-// function Nav() {
-//   return (
-//     <nav className={styles.container}>
-//       <ul className={styles["nav-links-ul"]}>
-//         <div className={styles["nav-links-left"]}>
-//           <li>
-//             <NavLink
-//               to="/doctors"
-//               className={({ isActive }) =>
-//                 isActive ? `${styles["link-active"]}` : `${styles.link}`
-//               }
-//             >
-//               Our doctors
-//             </NavLink>
-//           </li>
-//           <li>
-//             <NavLink
-//               to="/dry-eye-disease"
-//               className={({ isActive }) =>
-//                 isActive ? `${styles["link-active"]}` : `${styles.link}`
-//               }
-//             >
-//               Dry eye disease
-//             </NavLink>
-//           </li>
-//           <li>
-//             <NavLink
-//               to="/resources"
-//               className={({ isActive }) =>
-//                 isActive ? `${styles["link-active"]}` : `${styles.link}`
-//               }
-//             >
-//               Resources
-//             </NavLink>
-//           </li>
-//         </div>
-//         <div className={styles["nav-links-center"]}>
-//           <NavLink to="/">
-//             <img src={logo} className={styles.picture} />
-//           </NavLink>
-//         </div>
-//         <div className={styles["nav-links-right"]}>
-//           <li>
-//             <NavLink
-//               to="/at-home-treatments"
-//               className={({ isActive }) =>
-//                 isActive ? `${styles["link-active"]}` : `${styles.link}`
-//               }
-//             >
-//               At-home treatments
-//             </NavLink>
-//           </li>
-//           <li>
-//             <NavLink
-//               to="/in-office-treatments"
-//               className={({ isActive }) =>
-//                 isActive ? `${styles["link-active"]}` : `${styles.link}`
-//               }
-//             >
-//               In-office treatments
-//             </NavLink>
-//           </li>
-//           <li>
-//             <NavLink
-//               to="/contact"
-//               className={({ isActive }) =>
-//                 isActive ? `${styles["link-active"]}` : `${styles.link}`
-//               }
-//             >
-//               Contact us
-//             </NavLink>
-//           </li>
-//         </div>
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
